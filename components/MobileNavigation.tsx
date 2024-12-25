f8aed39e160f19187ba3d107f6ff0dd663eb970a
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-12-25 22:06:07 +0530

    adding signOutUser functionality in Header & MobileNavigation

--- components/MobileNavigation.tsx
+++ components/MobileNavigation.tsx
@@ -17,6 +17,7 @@ import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import FileUploader from "@/components/FileUploader";
+import { signOutUser } from "@/lib/actions/user.actions";
 
 interface Props {
   ownerId: string;
@@ -108,7 +109,7 @@ const MobileNavigation = ({
               <Button
                 type={"submit"}
                 className={"mobile-sign-out-button"}
-                onClick={() => {}}
+                onClick={async () => await signOutUser()}
               >
                 <Image
                   src={"/assets/icons/logout.svg"}
