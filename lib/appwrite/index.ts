08fbe32da727e58d1856ed7200a642bea577ff61
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-12-25 13:21:06 +0530

    adding create admin client for admin use

--- lib/appwrite/index.ts
+++ lib/appwrite/index.ts
@@ -22,3 +22,25 @@ export const createSessionClient = async () => {
     },
   };
 };
+
+export const createAdminClient = async () => {
+  const client = new Client()
+    .setEndpoint(appWriteConfig.endPointUrl)
+    .setProject(appWriteConfig.projectId)
+    .setKey(appWriteConfig.secretKey);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get databases() {
+      return new Databases(client);
+    },
+    get storage() {
+      return new Storage(client);
+    },
+    get avatars() {
+      return new Avatars(client);
+    },
+  };
+};
