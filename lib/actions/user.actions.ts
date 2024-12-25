58ea1f0bd685196148f91686faade4e0888c6d64
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-12-25 14:04:34 +0530

    adding createAccount function in user.actions

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -2,7 +2,13 @@
 
 import { createAdminClient } from "@/lib/appwrite";
 import { appWriteConfig } from "@/lib/appwrite/config";
-import { Query } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
+import { parseStringify } from "@/lib/utils";
+
+interface nameAndEmail {
+  fullName: string;
+  email: string;
+}
 
 const getUserByEmail = async (email: string) => {
   const { databases } = await createAdminClient();
@@ -14,4 +20,48 @@ const getUserByEmail = async (email: string) => {
   );
 
   return result.total > 0 ? result.documents[0] : null;
+};
+
+const handleError = (error: unknown, message: string) => {
+  console.log(error, message);
+  throw error;
+};
+
+const sendEmailOTP = async (email: string) => {
+  const { account } = await createAdminClient();
+
+  try {
+    const session = await account.createEmailToken(ID.unique(), email);
+
+    return session.userId;
+  } catch (error) {
+    handleError(error, "Failed to send email OTP.");
+  }
+};
+
+export const createAccount = async ({ fullName, email }: nameAndEmail) => {
+  const existingUser = await getUserByEmail(email);
+
+  const accountId = await sendEmailOTP(email);
+
+  if (!accountId) throw new Error("Failed to send an OTP.");
+
+  if (!existingUser) {
+    const { databases } = await createAdminClient();
+
+    await databases.createDocument(
+      appWriteConfig.databaseId,
+      appWriteConfig.usersCollectionId,
+      ID.unique(),
+      {
+        fullName,
+        email,
+        avatar:
+          "https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg",
+        accountId,
+      },
+    );
+  }
+
+  return parseStringify({ accountId });
 };
