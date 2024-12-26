5306a6bf2890e8ae7f322c4961a671f8640efa09
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-12-26 12:16:49 +0530

    adding convertFileToUrl in utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,6 +4,7 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+export const convertFileToUrl = (file: File) => URL.createObjectURL(file);
 
 export const parseStringify = (value: unknown) => {
   return JSON.parse(JSON.stringify(value));
