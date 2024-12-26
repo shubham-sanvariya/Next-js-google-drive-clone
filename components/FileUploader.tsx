b41577c6664f565f3e7916d2109f604f39e11db4
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-12-26 12:32:07 +0530

    adding Thumbnail component

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -1,6 +1,65 @@
-import React from "react";
+"use client";
 
-const FileUploader = () => {
-  return <div>FileUploader</div>;
+import React, { useCallback, useState } from "react";
+import { useDropzone } from "react-dropzone";
+import { Button } from "@/components/ui/button";
+import { cn, convertFileToUrl, getFileType } from "@/lib/utils";
+import Image from "next/image";
+import Thumbnail from "@/components/Thumbnail";
+
+interface Props {
+  ownerId: string;
+  accountId: string;
+  className?: string;
+}
+
+const FileUploader = ({ ownerId, accountId, className }: Props) => {
+  const [files, setFiles] = useState<File[]>([]);
+
+  const onDrop = useCallback(async (acceptedFiles: File[]) => {
+    setFiles(acceptedFiles);
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return (
+    <div {...getRootProps()} className={"cursor-pointer"}>
+      <input {...getInputProps()} />
+      <Button type={"button"} className={cn("uploader-button", className)}>
+        <Image
+          src={"/assets/icons/upload.svg"}
+          alt={"upload"}
+          width={24}
+          height={24}
+        />
+        <p>Upload</p>
+      </Button>
+      {files.length > 0 && (
+        <ul className="uploader-preview-list">
+          <h4 className={"h4 text-light-100"}>Uploading</h4>
+          {files.map((file, index) => {
+            const { type, extension } = getFileType(file.name);
+
+            return (
+              <li key={`${file.name}-${index}`}>
+                <div className={"flex items-center gap-3"}>
+                  <Thumbnail
+                    type={type}
+                    extension={extension}
+                    url={convertFileToUrl(file)}
+                  />
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+      {isDragActive ? (
+        <p>Drop the files here ...</p>
+      ) : (
+        <p>Drag &#39;n&#39; drop some files here, or click to select files</p>
+      )}
+    </div>
+  );
 };
 export default FileUploader;
