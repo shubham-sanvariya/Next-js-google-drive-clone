6ee7adf47e9afb17d1a1f9d9c6a9e3c5b756937f
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-12-27 13:01:06 +0530

    adding handleRemoveUser from shares

--- components/ActionDropdown.tsx
+++ components/ActionDropdown.tsx
@@ -64,7 +64,19 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
     setIsLoading(false);
   };
 
-  const handleRemoveUser = () => {};
+  const handleRemoveUser = async (email: string) => {
+    const updatedEmails = emails.filter((e) => e !== email);
+
+    const success = await updateFileUsers({
+      fileId: file.$id,
+      emails: updatedEmails,
+      path,
+    });
+
+    if (success) setEmails(updatedEmails);
+
+    closeAllModals();
+  };
 
   const renderDialogContent = () => {
     if (!action) return null;
